a69119ccebf0c4c267024ad1e5872d7943c32f10
kemalreza12 <kemalreza92@gmail.com>
2021-06-28 23:30:21 +0700

    layout home

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
+import './style-homepage.css'
 
 class Home extends Component {
     render() {
         return (
             <div>
-                <div class="container-fluid">
-                    <div class="row border">
-                        <div class="navbar col border">
-                            <div class="logo"><p>logo</p></div>
+                <div id="menu_screen" class="container-fluid main_container d-flex border">
+                    <div class="row h-100 border">
+                        <div class="navbar border">
+                            <div class="logo border"><p>logo</p></div>
+                            <div class="add border">
+                                <i class="fas fa-plus"></i>
+                            </div>
                         </div>
-                        <div class="col">
+                        <div class="col-sm h-100 border">
                             <h1>content</h1>
                         </div>
                     </div>
